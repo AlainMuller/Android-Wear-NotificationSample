3c20a20f78359a25cf8f3a753c81fb26a44056d1
Alain Muller <alain.muller@niji.fr>
2014-05-20 17:13:45 +0200

    Using string resource + adding comment

--- app/src/main/java/fr/alainmuller/wearnotificationsender/app/MainActivity.java
+++ app/src/main/java/fr/alainmuller/wearnotificationsender/app/MainActivity.java
@@ -1,11 +1,12 @@
 package fr.alainmuller.wearnotificationsender.app;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -26,33 +27,67 @@ public class MainActivity extends ActionBarActivity {
      * @param v le bouton
      */
     public void sendNotif(View v) {
-        Toast.makeText(this, "Notif envoyée! =)", Toast.LENGTH_SHORT).show();
 
-        // On crée un Intent permettant de lancer une Activity sur sélection de la notif
+        // On peut utiliser un identifiant de notification pour les distinguer lors de l'envoi au NotificationManager
+        int notificationId = 001;
+
+        // On crée un l'intent par défaut associé à la notification
         Intent intent = new Intent(this, SecondActivity.class);
         PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        // On peut créer autant d'Intet qu'on souhaite
+        // On peut créer autant d'Intent qu'on souhaite
+
+        // Un Intent de type Dial
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(Uri.parse("tel:" + getString(R.string.phone_number)));
+        PendingIntent pDialIntent = PendingIntent.getActivity(this, 0, dialIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Un Intent de type URL
         String url = "http://www.alainmuller.fr";
         Intent webIntent = new Intent(Intent.ACTION_VIEW);
         webIntent.setData(Uri.parse(url));
         PendingIntent pWebIntent = PendingIntent.getActivity(this, 0, webIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        // Construction de la notification
-        Notification n = new Notification.Builder(this)
-                .setContentTitle("Essai de notif")
-                .setContentText("Essai de notification vers l'émulateur Wear")
+        // Un Intent de type Map
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        Uri geoUri = Uri.parse("geo:0,0?q=" + Uri.encode(getString(R.string.location)));
+        mapIntent.setData(geoUri);
+        PendingIntent pMapIntent = PendingIntent.getActivity(this, 0, mapIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // On peut utiliser un style d'affichage en mode 'big view'
+        // afin d'afficher du texte trop long pour tenir dans le setContentText
+        NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
+        bigStyle.bigText(getString(R.string.lipsum));
+
+        // Construction de la notification (support des anciennes versions)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                // Titre de la notification
+                .setContentTitle(getString(R.string.notif_title))
+                        // setContentText remplacé par setStyle avec un bigText
+//                        .setContentText(getString(R.string.lipsum))
+                        // Icône miniature du system tray
                 .setSmallIcon(R.drawable.ic_notif_cloud)
+                        // Icône large visible quand on ouvre les notifications sur le téléphone
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
+                        // Affichage d'un bigText scrollable sur Wear et dans la description de la notif sur téléphone
+                .setStyle(bigStyle)
+                        // L'Intent par défaut
                 .setContentIntent(pIntent)
+                        // Cliquer sur la notif la détruit (non persistente)
                 .setAutoCancel(true)
-                .addAction(R.drawable.ic_notif_call, "Appeler", pIntent)
-                .addAction(R.drawable.ic_notif_net, "Site", pWebIntent)
-                .addAction(R.drawable.ic_notif_save, "Sauvegarder", pIntent).build();
+                        // Ajout de diverses actions
+                .addAction(R.drawable.ic_notif_call, getString(R.string.notif_action_call), pDialIntent)
+                .addAction(R.drawable.ic_notif_net, getString(R.string.notif_action_net), pWebIntent)
+                .addAction(R.drawable.ic_notif_nav, getString(R.string.notif_action_nav), pMapIntent)
+                .addAction(R.drawable.ic_notif_save, getString(R.string.notif_action_save), pIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0, n);
+        // On construit la notif à la volée
+        notificationManager.notify(notificationId, notificationBuilder.build());
+
+        Toast.makeText(this, "Notif envoyée! =)", Toast.LENGTH_SHORT).show();
 
     }
 }
